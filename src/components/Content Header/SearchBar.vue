c829045f100f840c48da2eb79b4a4b008953a954
Laurentiu <lcostin40@gmail.com>
2021-11-26 12:14:44 +0100

    Add img folder, logo and search bar to header

---
+++ src/components/Content Header/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="search">
+      <i class="fas fa-search"></i>
+      <input type="text" placeholder="Search..." />
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss">
+.search {
+  display: flex;
+  align-items: center;
+  background-color: #f3f4f3;
+  border: none;
+  padding: 15px;
+  border-radius: 50px;
+  input {
+    width: 300px;
+    border: none;
+    margin-left: 10px;
+    background-color: #f3f4f3;
+  }
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,2 +1,38 @@
-<template></template>
+<template>
+  <header>
+    <div class="cont_70 flex_space_between">
+      <div class="logo">
+        <img src="../assets/img/dark-pet-logo.png" alt="" />
+      </div>
+      <Search />
+    </div>
+  </header>
+</template>
 
+
+
+<script>
+import Search from "./Content Header/SearchBar.vue";
+export default {
+  name: "Header",
+
+  components: {
+    Search,
+  },
+};
+</script>
+
+
+
+
+<style lang="scss">
+.cont_70 {
+  width: 70%;
+  margin: 20px auto;
+}
+
+.flex_space_between {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
