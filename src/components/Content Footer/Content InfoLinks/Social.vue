305fe9257abb76a6ccf0b446ca85cc80bc448f9e
Laurentiu <lcostin40@gmail.com>
2021-11-29 17:02:54 +0100

    Add socials to footer

---
+++ src/components/Content Footer/Content InfoLinks/Social.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cont_social">
+    <nav>
+      <a v-for="link in links" :key="link" href="#">
+        {{ link }}
+      </a>
+    </nav>
+    <p>
+      <i class="fab fa-facebook-f"></i>
+      <i class="fab fa-twitter"></i>
+      <i class="fab fa-instagram"></i>
+      <i class="fab fa-youtube"></i>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Social",
+
+  data() {
+    return {
+      links: ["Home", "About", "Blog", "Contact", "Shop"],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.cont_social {
+  text-align: center;
+  padding: 50px 0 100px 0;
+
+  p {
+    color: #fff;
+
+    i {
+      margin-top: 50px;
+      margin-right: 20px;
+      font-size: 20px;
+    }
+  }
+}
+</style>
--- src/components/Content Footer/InfoLinks.vue
+++ src/components/Content Footer/InfoLinks.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="back_footer">
     <Links />
+    <Social />
   </div>
 </template>
 
 <script>
 import Links from "./Content InfoLinks/Links.vue";
+import Social from "./Content InfoLinks/Social.vue";
 
 export default {
   name: "InfoLinks",
 
   components: {
     Links,
+    Social,
   },
 };
 </script>
