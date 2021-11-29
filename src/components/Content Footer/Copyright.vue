c53152b5e9c98b616e88aa9c4abe95deb3b83cae
Laurentiu <lcostin40@gmail.com>
2021-11-29 17:20:28 +0100

    Add Copyright section to footer. End footer

---
+++ src/components/Content Footer/Copyright.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="copyright">
+    <p>
+      <i class="far fa-copyright"></i>
+      Copyright 2012 | Avada Theme by ThemeFusion | All Rights Reserved |
+      Powered by Lori
+    </p>
+    <div class="up" @click="topFunction()">
+      <i class="fas fa-angle-up"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Copyright",
+
+  methods: {
+    topFunction() {
+      /* console.log("Click"); */
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    },
+  },
+};
+</script>
+
+
+<style lang="scss">
+.copyright {
+  background-color: #000;
+  text-align: center;
+  position: relative;
+
+  p {
+    color: #fff;
+    padding: 20px 0;
+  }
+
+  .up {
+    position: absolute;
+    background-color: #2e2e2e;
+    color: #fff;
+    width: 50px;
+    height: 40px;
+    line-height: 40px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    bottom: 0;
+    right: 1%;
+    transform: translate(-99%, 0);
+  }
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,21 @@
 <template>
   <footer>
     <InfoLinks />
+    <Copyright />
   </footer>
 </template>
 
 
 <script>
 import InfoLinks from "./Content Footer/InfoLinks.vue";
+import Copyright from "./Content Footer/Copyright.vue";
 
 export default {
   name: "Footer",
 
   components: {
     InfoLinks,
+    Copyright,
   },
 };
 </script>
